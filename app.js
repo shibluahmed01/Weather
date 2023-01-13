e3ecb85842dbd740ef4406295309b9afd56e3d94
shibluahmed01 <mdshibluahmed01@gmail.com>
2023-01-12 13:59:43 -1200

    weather

---
+++ app.js
@@ -0,0 +1,19 @@
+const submitBtn = document.getElementById("submit-btn");
+submitBtn.addEventListener("click", function() {
+    const inputbtn = document.getElementById("input-btn").value;
+
+    fetch(
+            `http://api.openweathermap.org/data/2.5/weather?q=${inputbtn}&appid=4450c049dc640f47383e2b1a34a897df`
+        )
+        .then((Response) => Response.json())
+        .then((data) => {
+            const cityName = data.name;
+            const tem = data.main.temp;
+            const temp = tem - 273.15;
+            const description = data.weather[0].description;
+
+            document.getElementById("city").innerText = cityName;
+            document.getElementById("temp").innerText = temp.toFixed(2);
+            document.getElementById("des").innerText = description;
+        });
+});
